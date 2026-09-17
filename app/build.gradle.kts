1469c00384afddab56f7dfe35095d8307a8e2fd2
alonbz <alonbz@gmail.com>
2026-09-17 13:45:52 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.agsense.soilsensor7in1"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.6.0"
+        versionCode = 14
+        versionName = "1.6.1"
     }
 
     signingConfigs {
